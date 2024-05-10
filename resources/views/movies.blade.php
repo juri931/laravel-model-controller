6800cbca35ab3314f883c1558f6d70193888b527
juri931 <impa.mat@hotmail.it>
2024-05-10 17:08:02 +0200

    [add] background cards images

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -4,8 +4,9 @@
     <h1>I miei film</h1>
     <div class="container">
         @foreach ($movies as $movie)
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
+        <div class="card bg-dark text-white" style="width: 18rem;">
+            <img style="background-image: url({{ $movie->image }})" class="card-img">
+            <div class="card-img-overlay">
                 <h5 class="card-title">{{ $movie->title }}</h5>
                 <p class="card-text">Titolo originale: {{ $movie->original_title }}</p>
                 <p class="card-text">Nazionalità: {{ $movie->nationality }}</p>
